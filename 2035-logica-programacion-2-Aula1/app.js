01473aa22105023904921a6c463a033a21befabb
Daniel Cubides <111407907+dcc1967@users.noreply.github.com>
2024-07-20 20:14:10 -0500

    Version final aula1

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+/*let parrafo = document.querySelector('P');
+titulo.innerHTML = 'indica un numero del 1 al 10';*/
+
+function intentoDeUsuario() {
+    alert("click desde el boton");
+}
